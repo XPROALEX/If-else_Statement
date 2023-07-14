d5d969b7c89ec1eb0e7b8bd2b549c3c4b2a8e1ba
Alex <alex97av@gmail.com>
2023-07-14 17:05:41 +0200

    pulizia codice

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         String myName = "Alessandro";
         if (myName.length() % 2 == 0) {
             System.out.println("the number of letters making up the name is Even");
-        } else { System.out.println("the number of letters making up the name is Odd");
-        }
+        } else System.out.println("the number of letters making up the name is Odd");
     }
 }
