4a6877d65c10ad5688da8d078d5a9da7dd5ab212
Alex <alex97av@gmail.com>
2023-07-14 17:02:35 +0200

    creazione if-else con out(esercizio completato)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-    String myName="Alessandro";
-}
+    public static void main(String[] args) {
+        String myName = "Alessandro";
+        if (myName.length() % 2 == 0) {
+            System.out.println("the number of letters making up the name is Even");
+        } else { System.out.println("the number of letters making up the name is Odd");
+        }
+    }
 }
